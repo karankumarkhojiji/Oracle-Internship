93f5cd1056e6f12047befe4c0b030d13d5d5bffc
karankumarkhojiji <khojijikaran@gmail.com>
2023-06-07 17:13:10 +0530

    transactionlist procedure added

--- Week-2.sql
+++ Week-2.sql
@@ -2,104 +2,83 @@
 
 create or replace procedure transactionlist(
     p_customer_id in number default null,
-    p_transaction_type in varchar2 default null
+    p_transaction_type in varchar2 default null,
+    p_results out varchar2
 ) as
+    v_result varchar2(4000); -- variable to store the result
 begin
     if p_customer_id is not null and p_transaction_type is not null then
-        -- Retrieve transactions for a specific customer and transaction type
-        for trans in (
-            select t.*, c.first_name, c.last_name, c.email
-            from transactions t
-                     join customers c on t.customer_id = c.customer_id
-            where t.customer_id = p_customer_id
-              and t.transactions_type = p_transaction_type
-            )
-            loop
-                dbms_output.put_line('Transaction ID: ' || trans.transaction_id);
-                dbms_output.put_line('Transaction Date: ' || trans.transactions_date);
-                dbms_output.put_line('Transaction Type: ' || trans.transactions_type);
-                dbms_output.put_line('Amount: ' || trans.amount);
-                dbms_output.put_line('Customer ID: ' || trans.customer_id);
-                dbms_output.put_line('Equity ID: ' || trans.equity_id);
-                dbms_output.put_line('First Name: ' || trans.first_name);
-                dbms_output.put_line('Last Name: ' || trans.last_name);
-                dbms_output.put_line('Email: ' || trans.email);
-                dbms_output.put_line('-----------------------------');
-            end loop;
+        -- retrieve transactions for a specific customer and transaction type
+        select 'transaction id: ' || t.transaction_id || chr(10) ||
+               'transaction date: ' || t.transactions_date || chr(10) ||
+               'transaction type: ' || t.transactions_type || chr(10) ||
+               'amount: ' || t.amount || chr(10) ||
+               'customer id: ' || t.customer_id || chr(10) ||
+               'equity id: ' || t.equity_id || chr(10) ||
+               'first name: ' || c.first_name || chr(10) ||
+               'last name: ' || c.last_name || chr(10) ||
+               'email: ' || c.email
+        into v_result
+        from transactions t
+                 join customers c on t.customer_id = c.customer_id
+        where t.customer_id = p_customer_id
+          and t.transactions_type = p_transaction_type;
+
     elsif p_customer_id is not null then
-        -- Retrieve transactions for a specific customer
-        for trans in (
-            select t.*, c.first_name, c.last_name, c.email
-            from transactions t
-                     join customers c on t.customer_id = c.customer_id
-            where t.customer_id = p_customer_id
-            )
-            loop
-                dbms_output.put_line('Transaction ID: ' || trans.transaction_id);
-                dbms_output.put_line('Transaction Date: ' || trans.transactions_date);
-                dbms_output.put_line('Transaction Type: ' || trans.transactions_type);
-                dbms_output.put_line('Amount: ' || trans.amount);
-                dbms_output.put_line('Customer ID: ' || trans.customer_id);
-                dbms_output.put_line('Equity ID: ' || trans.equity_id);
-                dbms_output.put_line('First Name: ' || trans.first_name);
-                dbms_output.put_line('Last Name: ' || trans.last_name);
-                dbms_output.put_line('Email: ' || trans.email);
-                dbms_output.put_line('-----------------------------');
-            end loop;
+        -- retrieve transactions for a specific customer
+        select 'transaction id: ' || t.transaction_id || chr(10) ||
+               'transaction date: ' || t.transactions_date || chr(10) ||
+               'transaction type: ' || t.transactions_type || chr(10) ||
+               'amount: ' || t.amount || chr(10) ||
+               'customer id: ' || t.customer_id || chr(10) ||
+               'equity id: ' || t.equity_id || chr(10) ||
+               'first name: ' || c.first_name || chr(10) ||
+               'last name: ' || c.last_name || chr(10) ||
+               'email: ' || c.email
+        into v_result
+        from transactions t
+                 join customers c on t.customer_id = c.customer_id
+        where t.customer_id = p_customer_id;
+
     elsif p_transaction_type is not null then
-        -- Retrieve transactions for a specific transaction type
-        for trans in (
-            select t.*, c.first_name, c.last_name, c.email
-            from transactions t
-                     join customers c on t.customer_id = c.customer_id
-            where t.transactions_type = p_transaction_type
-            )
-            loop
-                dbms_output.put_line('Transaction ID: ' || trans.transaction_id);
-                dbms_output.put_line('Transaction Date: ' || trans.transactions_date);
-                dbms_output.put_line('Transaction Type: ' || trans.transactions_type);
-                dbms_output.put_line('Amount: ' || trans.amount);
-                dbms_output.put_line('Customer ID: ' || trans.customer_id);
-                dbms_output.put_line('Equity ID: ' || trans.equity_id);
-                dbms_output.put_line('First Name: ' || trans.first_name);
-                dbms_output.put_line('Last Name: ' || trans.last_name);
-                dbms_output.put_line('Email: ' || trans.email);
-                dbms_output.put_line('-----------------------------');
-            end loop;
+        -- retrieve transactions for a specific transaction type
+        select 'transaction id: ' || t.transaction_id || chr(10) ||
+               'transaction date: ' || t.transactions_date || chr(10) ||
+               'transaction type: ' || t.transactions_type || chr(10) ||
+               'amount: ' || t.amount || chr(10) ||
+               'customer id: ' || t.customer_id || chr(10) ||
+               'equity id: ' || t.equity_id || chr(10) ||
+               'first name: ' || c.first_name || chr(10) ||
+               'last name: ' || c.last_name || chr(10) ||
+               'email: ' || c.email
+        into v_result
+        from transactions t
+                 join customers c on t.customer_id = c.customer_id
+        where t.transactions_type = p_transaction_type;
+
     else
-        -- Retrieve all transactions for all customers
-        for trans in (
-            select t.*, c.first_name, c.last_name, c.email
-            from transactions t
-                     join customers c on t.customer_id = c.customer_id
-            )
-            loop
-                dbms_output.put_line('Transaction ID: ' || trans.transaction_id);
-                dbms_output.put_line('Transaction Date: ' || trans.transactions_date);
-                dbms_output.put_line('Transaction Type: ' || trans.transactions_type);
-                dbms_output.put_line('Amount: ' || trans.amount);
-                dbms_output.put_line('Customer ID: ' || trans.customer_id);
-                dbms_output.put_line('Equity ID: ' || trans.equity_id);
-                dbms_output.put_line('First Name: ' || trans.first_name);
-                dbms_output.put_line('Last Name: ' || trans.last_name);
-                dbms_output.put_line('Email: ' || trans.email);
-                dbms_output.put_line('-----------------------------');
-            end loop;
+        -- retrieve all transactions for all customers
+        select 'transaction id: ' || t.transaction_id || chr(10) ||
+               'transaction date: ' || t.transactions_date || chr(10) ||
+               'transaction type: ' || t.transactions_type || chr(10) ||
+               'amount: ' || t.amount || chr(10) ||
+               'customer id: ' || t.customer_id || chr(10) ||
+               'equity id: ' || t.equity_id || chr(10) ||
+               'first name: ' || c.first_name || chr(10) ||
+               'last name: ' || c.last_name || chr(10) ||
+               'email: ' || c.email
+        into v_result
+        from transactions t
+                 join customers c on t.customer_id = c.customer_id;
     end if;
+
+    -- assign the result to the output parameter
+    p_results := v_result;
 end;
 
-begin
-    transactionList(p_customer_id =>5, p_transaction_type => 'Buy');
-end;
 
+declare
+    v_results varchar2(4000);
 begin
-    transactionList(p_customer_id =>5);
-end;
-
-begin
-    transactionList(p_transaction_type => 'Buy');
-end;
-
-begin
-    transactionList();
+    transactionlist(p_customer_id => 5, p_transaction_type => 'Buy', p_results => v_results);
 end;
